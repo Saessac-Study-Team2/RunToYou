179177eef51a6e1f17b9e1ca6545895a0bf76c35
Swan <adeliine.0618@gmail.com>
2022-06-22 23:30:45 +0900

    feat: Update Mypage component

--- src/pages/myPage.jsx
+++ src/pages/myPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Profile from '../components/profile';
+// import Filtering from '../components/filtering';
 
-const MyPage = () => {
+const MyPage = ({ posts, setPosts }) => {
+  const [selected, setSelected] = useState('all');
+
+  const selectBoxChange = event => {
+    setSelected(event.target.value);
+  };
+
+  // const selectedPosts = posts.filter(post => {
+  //   return post.username === selected;
+  // });
+
   return (
     <>
       <h1>Mypage</h1>
+      <Profile />
+      {/* <Filtering
+        selected={selected}
+        setSelected={setSelected}
+        selectBoxChange={selectBoxChange}
+      /> */}
+      {/* <ul className='myPosts'>
+        {selected === 'all' //처음이면 전부 다 보여줘.
+          ? posts.map(post => {
+              return <Post />;
+            })
+          : selectedPosts.map(post => {
+              //처음 아니라면 셀렉된 옵션값거
+              return <Post />;
+            })}
+      </ul> */}
     </>
   );
 };
