a1a95dae8a8ebce0bee531942458928edc7e2154
Swan <99741801+adeline0618@users.noreply.github.com>
2022-06-27 15:18:08 +0900

    :sparkles: add form to editProfile

--- src/components/editProfile.jsx
+++ src/components/editProfile.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
-const EditProfile = props => {
+const EditProfile = ({ addProfile }) => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(
+      event.target[0].value,
+      event.target[1].value,
+      event.target[2].value
+    );
+    const nickname = event.target[0].value;
+    const preferPlace = event.target[1].value;
+    const aboutMe = event.target[2].value;
+    addProfile({ nickname, preferPlace, aboutMe });
+  };
+
   return (
     <section className='edit__profile'>
-      <div className='profile__img'>
-        <input type='text'>img</input>
-      </div>
-      <div className='profile__content'>
-        <input type='text' className='profile__name'></input>
-        <input type='text' className='profile__myplace'></input>
-        <input type='text' className='profile__introduce'></input>
-      </div>
+      <form action='' method='get' className='form' onSubmit={handleSubmit}>
+        <div className='profile__img'>이미지 인풋</div>
+        <div className='profile__content'>
+          <label htmlFor='title'>Enter your nickname </label>
+          <input type='text' name='name' id='name' required />
+          <label htmlFor='title'>Enter your favorite place </label>
+          <input type='text' name='title' id='title' required />
+          <textarea
+            id='story'
+            name='story'
+            placeholder='Please Write about you'
+            required
+          ></textarea>
+          <input type='submit' value='submit' />
+        </div>
+      </form>
     </section>
   );
 };
