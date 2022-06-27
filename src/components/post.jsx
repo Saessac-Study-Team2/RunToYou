2e26d41c3c2ccc7748b184f074bfe973866c0188
Swan <adeliine.0618@gmail.com>
2022-06-27 11:58:49 +0900

    :recycle: Refactor MyPage

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
 // MainPage에 posts state에서 데이터를 받아서 게시글 하나를 만드는 컴포넌트
 const Post = ({ post }) => {
   return (
-    <li className="post" id={post.id}>
-      <div className="userInfo">
-        <span className="userName">{post.username}</span>
-        <span className="post__createAt"></span>
+    <li className='post' id={post.id}>
+      <div className='userInfo'>
+        <span className='userName'>{post.username}</span>
+        <span className='post__createAt'></span>
       </div>
-      <div className="postInfo">
-        <p className="postContent">{post.content}</p>
+      <div className='postInfo'>
+        <p className='postContent'>{post.content}</p>
       </div>
     </li>
   );
