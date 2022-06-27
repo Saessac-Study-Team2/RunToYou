4ff81183f3d390de4ead00b8ea8cb5fd54a164a8
Swan <99741801+adeline0618@users.noreply.github.com>
2022-06-27 15:00:24 +0900

    :sparkles: add editProfile function

--- src/pages/myPage.jsx
+++ src/pages/myPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import { Tab, Tabs } from '@material-ui/core';
 
@@ -8,6 +8,42 @@ import EditProfile from '../components/editProfile';
 // import Filtering from '../components/filtering';
 
 const MyPage = ({ posts, setPosts }) => {
+  const domain = 'http://34.168.215.145';
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const getProfile = id => {
+    return fetch(domain + `/user/1`)
+      .then(res => res.json())
+      .then(data => {
+        setProfile(data);
+      });
+  };
+
+  const addProfile = ({ nickname, preferPlace, aboutMe }) => {
+    const myProfile = {
+      nickname,
+      preferPlace,
+      aboutMe,
+    };
+
+    fetch(domain + `/user/asd`, {
+      method: 'PUT',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(myProfile),
+    }).then(res => {
+      if (res.status === 201) {
+        getProfile();
+      }
+    });
+    console.log(myProfile, JSON.stringify(myProfile));
+  };
   return (
     <>
       <Tabs>
@@ -17,7 +53,10 @@ const MyPage = ({ posts, setPosts }) => {
       <Profile />
       <MyPost />
       <Routes>
-        <Route path='editprofile' element={<EditProfile />}></Route>
+        <Route
+          path='editprofile'
+          element={<EditProfile addProfile={addProfile} />}
+        ></Route>
       </Routes>
     </>
   );
