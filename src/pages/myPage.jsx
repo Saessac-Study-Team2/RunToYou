d3e2a9bbdd90aba08cbcd374b557dcf6eb93b2a5
Swan <adeliine.0618@gmail.com>
2022-07-05 00:57:02 +0900

    ♻️인증토큰 쿠키 저장

--- src/pages/myPage.jsx
+++ src/pages/myPage.jsx
@@ -1,62 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import { Tab, Tabs } from '@material-ui/core';
-
 import Profile from '../components/profile';
 import MyPost from '../components/myPost';
-import EditProfile from '../components/editProfile';
-// import Filtering from '../components/filtering';
+import { getLoginCookie } from '../library/cookie';
+const axios = require('axios');
 
 const MyPage = ({ posts, setPosts }) => {
-  const domain = 'http://34.168.215.145';
-  const [user, setUser] = useState({});
+  const [isLogin, setLogin] = useState(false);
+  const [list, setList] = useState([]);
 
-  const getProfile = number => {
-    // const requestOptions = {
-    //   method: 'GET',
-    //   redirect: 'follow',
-    // };
-
-    return fetch(`http://34.168.215.145/user/${number}`, { method: 'GET' })
-      .then(response => response.json())
-      .then(result => {
-        console.log('getres', result);
-        setUser(result);
+  const getProfile = () => {
+    return axios
+      .get('http://34.168.215.145/user/', {
+        headers: { Authorization: getLoginCookie() },
+      })
+      .then(res => {
+        setLogin(res.data.msg);
+        if (res.data.msg) {
+          setList(Object.values(res.data.data));
+        }
       })
       .catch(error => console.log('error', error));
   };
 
-  const addProfile = ({ nickname, info }) => {
-    const user = {
-      nickname,
+  const addProfile = ({ nickName, info }) => {
+    const data = {
+      nickname: nickName,
       info,
     };
-    fetch(domain + `/user/1`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    }).then(res => {
-      if (res.status === 200) {
-        getProfile(1);
-      }
-    });
+    console.log(data);
+    axios
+      .put(`http://34.168.215.145/user`, data, {
+        headers: { Authorization: getLoginCookie() },
+      })
+      .then(getProfile())
+      .catch(error => console.log('error', error));
   };
 
   useEffect(() => {
-    getProfile(1);
+    getProfile();
   }, []);
+
   return (
     <>
-      <Profile user={user} />
-      <MyPost />
-      <Routes>
-        <Route
-          path='editprofile'
-          element={<EditProfile addProfile={addProfile} />}
-        ></Route>
-      </Routes>
+      {isLogin ? (
+        <>
+          <Profile list={list} setList={setList} addProfile={addProfile} />
+          <MyPost />
+        </>
+      ) : (
+        <p>로그인을 해주세요.</p>
+      )}
     </>
   );
 };
