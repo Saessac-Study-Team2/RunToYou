d923b7e50dea9bcfc8fc9ccaad25e8d601588cb4
dukjjang <jhd7292@naver.com>
2022-07-26 16:27:08 +0900

    :lipstick: 게시판 css 수정

--- src/pages/mainpage/mainPage.jsx
+++ src/pages/mainpage/mainPage.jsx
@@ -1,14 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Weather from "./weather/weather";
 import Posts from "./posts/posts";
-import WriteModal from "../../components/Modals/writeModal";
 import Header from "../../components/header/header";
 import styles from "./mainPage.module.css";
 import Footer from "../../components/footer/footer";
-
 import { useRecoilState } from "recoil";
 import {
-  isUserState,
   postsState,
   locationListState,
   userIDState,
@@ -18,12 +15,11 @@ import { getPosts } from "../../library/axios";
 
 const axios = require("axios");
 const MainPage = ({ isUser, setIsUser }) => {
-  const [isLogin, setIsLogin] = useRecoilState(isUserState);
   const [posts, setPosts] = useRecoilState(postsState);
-  const [modalOpen, setModalOpen] = useState(false);
   const [locationList, setLocationList] = useRecoilState(locationListState);
   const [userId, setUserId] = useRecoilState(userIDState);
   const [postsLength, setPostsLength] = useRecoilState(postsLengthState);
+
   //장소 리스트 가져오기
   useEffect(() => {
     axios
@@ -38,33 +34,17 @@ const MainPage = ({ isUser, setIsUser }) => {
   useEffect(() => {
     getPosts()
       .then((data) => {
-        setPosts(data);
+        setPosts(data.reverse());
         setPostsLength(data.length);
       })
       .catch((error) => console.log("error", error));
   }, []);
-
-  //모달
-  const openModal = () => {
-    setModalOpen(true);
-  };
-  const closeModal = () => {
-    setModalOpen(false);
-  };
 
   return (
     <>
       <div className={styles.mainPage}>
         <Header isUser={isUser} setIsUser={setIsUser} />
         <div className={styles.main}>
-          {isLogin && <button onClick={openModal}>글쓰기</button>}
-          <WriteModal
-            locationList={locationList}
-            open={modalOpen}
-            close={closeModal}
-            header="글쓰기"
-            setPosts={setPosts}
-          ></WriteModal>
           <div className={styles.mainContents}>
             <Weather />
             <Posts
--- src/pages/mainpage/serch/search.jsx
+++ src/pages/mainpage/serch/search.jsx
@@ -1,6 +1,9 @@
 import "./search.css";
 import { useRecoilState } from "recoil";
 import { postsState, isUserState } from "../../../library/atom";
+import WriteModal from "../../../components/Modals/writeModal";
+import { useState } from "react";
+import styles from "./search.module.css";
 
 const Search = ({
   posts,
@@ -13,10 +16,21 @@ const Search = ({
   myPosts,
   setMyPosts,
   setPosts,
+  locationList,
 }) => {
   const [isLogin, setIsLogin] = useRecoilState(isUserState);
   const locationNameList = ["강동구", "관악구", "마포구"];
   const recruitList = ["모집중", "모집완료"];
+  const [modalOpen, setModalOpen] = useState(false);
+
+  //모달
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   const handleSelect = (e) => {
     setSelected(e.target.value);
   };
@@ -38,50 +52,76 @@ const Search = ({
     }
   }
   return (
-    <section className="search__container">
-      <select
-        onChange={handleSelect}
-        value={selected}
-        className="search__place"
-      >
-        <option value={"지역검색"}>--지역--</option>
-        {locationNameList.map((el, idx) => {
-          return (
-            <option key={idx} value={el}>
-              {el}
-            </option>
-          );
-        })}
-      </select>
-      <select
-        onChange={handleRecruit}
-        value={recruit}
-        className="search__recruit"
-      >
-        <option value={"모집상태"}>--모집상태--</option>
-        {recruitList.map((el, idx) => {
-          return (
-            <option
-              key={idx}
-              value={el === "모집중" ? "recruiting" : "recruited"}
+    <>
+      <section className={styles.search__container}>
+        <div className={styles.container__col1}>
+          {isLogin && (
+            <button className={styles.writeBtn} onClick={openModal}>
+              글쓰기
+            </button>
+          )}
+          {isLogin ? (
+            <button
+              className={styles.myPosts}
+              onClick={handleMyPosts}
+              type="button"
             >
-              {el}
-            </option>
-          );
-        })}
-      </select>
-      <input
-        value={writer}
-        onChange={handleWriter}
-        placeholder="작성자"
-        className="search__writer"
-      ></input>
-      {isLogin ? (
-        <button onClick={handleMyPosts} type="button">
-          {myPosts === true ? "전체보기" : "내글보기"}
-        </button>
-      ) : null}
-    </section>
+              {myPosts === true ? "전체보기" : "내글보기"}
+            </button>
+          ) : null}
+          <WriteModal
+            locationList={locationList}
+            open={modalOpen}
+            close={closeModal}
+            header="글쓰기"
+            setPosts={setPosts}
+          ></WriteModal>
+        </div>
+        <div className={styles.container__col2}>
+          <div className={styles.selectBox}>
+            <select
+              onChange={handleSelect}
+              value={selected}
+              className={styles.select1}
+            >
+              <option value={"지역검색"}>--지역--</option>
+              {locationNameList.map((el, idx) => {
+                return (
+                  <option key={idx} value={el}>
+                    {el}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className={styles.selectBox}>
+            <select
+              onChange={handleRecruit}
+              value={recruit}
+              className={styles.select2}
+            >
+              <option value={"모집상태"}>--모집상태--</option>
+              {recruitList.map((el, idx) => {
+                return (
+                  <option
+                    key={idx}
+                    value={el === "모집중" ? "recruiting" : "recruited"}
+                  >
+                    {el}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <input
+            value={writer}
+            onChange={handleWriter}
+            placeholder="작성자"
+            className={styles.writer}
+          ></input>
+        </div>
+      </section>
+    </>
   );
 };
 
