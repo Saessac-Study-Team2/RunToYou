2b763a3a80af8d11b238526a6852018e30101db1
Swan <99741801+adeline0618@users.noreply.github.com>
2022-07-28 11:49:04 +0900

    :recycle: recoil-persist

--- src/library/atom.js
+++ src/library/atom.js
@@ -1,50 +1,58 @@
-import { atom } from "recoil";
+import { atom } from 'recoil';
+
+import { recoilPersist } from 'recoil-persist';
+const { persistAtom } = recoilPersist({
+  key: 'User',
+  storage: localStorage,
+});
 
 export const userState = atom({
-  key: "userState",
+  key: 'userState',
   default: {},
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const UserAvataState = atom({
-  key: "userAvataState",
+  key: 'userAvataState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
 export const userPlaceState = atom({
-  key: "userPlaceState",
+  key: 'userPlaceState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const userIDState = atom({
-  key: "userIDState",
+  key: 'userIDState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
 export const nicknameState = atom({
-  key: "nicknameState",
+  key: 'nicknameState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
 export const aboutMeState = atom({
-  key: "aboutMeState",
+  key: 'aboutMeState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const isUserState = atom({
-  key: "isUserState",
+  key: 'isUserState',
   default: null,
 });
 
 export const postsState = atom({
-  key: "PostsState",
+  key: 'PostsState',
   default: [],
 });
 export const locationListState = atom({
-  key: "LocationListState",
+  key: 'LocationListState',
   default: [],
-
 });
 export const postsLengthState = atom({
-  key: "postsLengthState",
+  key: 'postsLengthState',
   default: 0,
 });
-
-
-
