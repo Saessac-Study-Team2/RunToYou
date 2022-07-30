d127928212fa63dd1b571aa0dde16d3d1d80aa08
dukjjang <jhd7292@naver.com>
2022-07-31 07:46:08 +0900

    :recycle: 내글보기 수정

--- src/pages/mainpage/posts/posts.jsx
+++ src/pages/mainpage/posts/posts.jsx
@@ -20,23 +20,23 @@ const Posts = ({ locationList, userId, users }) => {
 
   for (let i = 0; i < posts.length; i++) {
     for (let key in posts[i]) {
-      if (key === 'recruit' && recruit !== '모집상태') {
-        filteredPosts = filteredPosts.filter(x => {
+      if (key === "recruit" && recruit !== "모집상태") {
+        filteredPosts = filteredPosts.filter((x) => {
           return x[key] === recruit;
         });
       }
-      if (key === 'locationName' && selected !== '지역검색') {
-        filteredPosts = filteredPosts.filter(x => {
+      if (key === "locationName" && selected !== "지역검색") {
+        filteredPosts = filteredPosts.filter((x) => {
           return x[key] === selected;
         });
       }
-      if (key === 'userID' && writer !== '') {
-        filteredPosts = filteredPosts.filter(x => {
+      if (key === "userID" && writer !== "") {
+        filteredPosts = filteredPosts.filter((x) => {
           return x[key].indexOf(writer) !== -1;
         });
       }
-      if (key === 'userID' && myPosts !== false) {
-        filteredPosts = filteredPosts.filter(x => {
+      if (key === "userID" && myPosts !== false) {
+        filteredPosts = filteredPosts.filter((x) => {
           return x[key] === userId;
         });
       }
